6746cf7dc772b145e8357911a8d070128afcfadb
yazeedhammad <yazeedhammad91@gmail.com>
2019-03-26 23:24:22 +0200

    (add) client side

---
+++ server/routes/api.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require('../models/user');
+
+const mongoose = require('mongoose')
+const db = "mongodb://techno:<techno123>@ds125486.mlab.com:25486/technoliege-task"
+
+mongoose.connect(db, err => {
+    if (err) {
+        console.error('Error!' + err)
+    } else {
+        console.log('Connected To MongoDB')
+    }
+})
+
+router.get('/', (req, res) => {
+    res.send('From API Route')
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            let payload = { subject: registeredUser._id }
+            let token = jwt.sign(payload, 'secretkey')
+            res.status(200).send({token});
+        }
+    })
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+    User.findOne({userName: userData.userName}, (error, user) => {
+        if (error) {
+            console.log(error);
+        } else {
+        if ( !user ) {
+            res.status(401).send('Invalid UserName');
+        } else 
+        if ( user.password !== userData.password) {
+            res.status(401).send('Invalid Password');
+        } else {
+            let payload = { subject: user._id }
+            let token = jwt.sign(payload, 'secretkey')
+            res.status(200).send({token});
+        }
+    }
+    })
+})
+
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
- const PORT =  3000;
+const PORT =  3000;
 const api = require('./routes/api');
 const app = express();
-app.use(express.static('./ngApp/dist/ngApp'));
 
-app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname, './ngApp/dist/ngApp/index.html'))
-})
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -19,8 +15,6 @@ app.use('/api', api)
 app.get('/', function(req, res) {
     res.send('Hello From Server')
 })
-
-
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Server Running on Localhost: ' + PORT)
