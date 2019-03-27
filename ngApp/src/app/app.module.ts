9431a863b4fd38d20aa9607f897e78eb012ee77c
yazeedhammad <yazeedhammad91@gmail.com>
2019-03-27 12:36:05 +0200

    (add) nav bar

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
+    NavComponent,
     ProfileComponent,
-    NewsComponent
+    NewsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
